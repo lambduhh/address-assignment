82fc973dbfb67cf87d4305bb45d8b5a1ac9a52a4
lorilynjmiller <lorilynjmiller@gmail.com>
2019-12-12 16:21:51 -0500

    -intial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to address-assignment
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/address_assignment/core_test.clj
@@ -0,0 +1,7 @@
+(ns address-assignment.core-test
+  (:require [clojure.test :refer :all]
+            [address-assignment.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
