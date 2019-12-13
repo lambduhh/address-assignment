2d032742e7d1bc082d8de75ae7bd1768805bc291
lorilynjmiller <lorilynjmiller@gmail.com>
2019-12-13 18:27:24 -0500

    - building string manipulation logic

--- src/address_assignment/core.clj
+++ src/address_assignment/core.clj
@@ -1,6 +1,15 @@
-(ns address-assignment.core)
+(ns address-assignment.core
+  (:require [libpython-clj.python :as py]
+            ))
+#_(py/initialize! :python-home "/usr/local/bin/python3.7"
+                :library-path "")
+
+;; (py/initialize!)
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+
+
---
+++ src/address_assignment/parse_and_sort.clj
@@ -0,0 +1,52 @@
+(ns address-assignment.parse-and-sort
+  (:require [clojure.string :as str]))
+
+(def comma
+  (slurp "/home/lorilynjmiller/IdeaProjects/address-assignment/resources/comma.txt"))
+
+(def pipe
+  (slurp "/home/lorilynjmiller/IdeaProjects/address-assignment/resources/pipe.txt"))
+
+(def spaces
+  (slurp "/home/lorilynjmiller/IdeaProjects/address-assignment/resources/tabs.txt"))
+
+(defn split-by-line [txt]
+  (clojure.string/split-lines txt))
+
+
+(defn comma-separated? [txt]
+  (let [chars (seq txt)]
+    (= 4 (count (filter #{\,} chars)))))
+
+(defn pipe-separated? [txt]
+  (let [chars (seq txt)]
+    (= 4 (count (filter #{\|} chars)))))
+
+(defn confusing? [txt]
+  (let [chars (seq txt)]
+    (and (comma-separated? txt) (pipe-separated? txt))))
+
+(comment
+  (def txt (first (split-by-line pipe)))
+  (pipe-separated? txt)
+  )
+
+
+(defn line-based-dispatch-fn [line]
+  (cond
+    (confusing? line) :error
+    (pipe-separated? line) :pipe
+    (comma-separated? line) :comma
+    :else :space))
+
+(defn split-by-delimiter [string delimiter]
+  ;; (str/split string )
+  )
+
+
+(comment
+  (pipe-separated? (split-by-line pipe))
+  (split-by-line pipe)
+  (split-by-line comma)
+
+  )
