77019483e8c117497905d2efb17c474d1bd3985a
lorilynjmiller <lorilynjmiller@gmail.com>
2019-12-14 17:31:34 -0500

    -finished logic to concat all files

--- src/address_assignment/parse_and_sort.clj
+++ src/address_assignment/parse_and_sort.clj
@@ -13,6 +13,7 @@
 (defn split-by-line [txt]
   (clojure.string/split-lines txt))
 
+; build predicates for dispatch function
 (defn comma-separated? [txt]
   (let [chars (seq txt)]
     (= 4 (count (filter #{\,} chars)))))
@@ -25,9 +26,11 @@
   (let [chars (seq txt)]
     (and (comma-separated? txt) (pipe-separated? txt))))
 
+;test predicate functions on line
 (comment
-  (def txt (first (split-by-line pipe)))
+  (def txt (first (split-by-line comma)))
   (pipe-separated? txt)
+  (comma-separated? txt)
   )
 
 (defn line-based-dispatch-fn [line]
@@ -37,14 +40,63 @@
     (confusing? line) :error
     :else :space))
 
-(defn split-by-delimiter [string delimiter]
-  ;; (str/split string )
-  )
+(defmulti process-line line-based-dispatch-fn)
 
+(defmethod process-line :pipe [line]
+  (let [[last-name first-name gender fcolor dob :as line] (str/split line #"[|]")] ;need to escape pipe in regex w brackets bc pipe MEANS SOMETHING in regex
+    ; WYSIWYG
+    {:type     :pipe
+     :last     last-name
+     :first    first-name
+     :gender   gender
+     :favcolor fcolor
+     :dob      dob}))
+
+(defmethod process-line :comma [line]
+  (let [[last-name first-name gender fcolor dob] (str/split line #"[,]")]
+    {:type     :comma
+     :last     last-name
+     :first    first-name
+     :gender   gender
+     :favcolor fcolor
+     :dob      dob}))
+
+(defmethod process-line :error [line]
+  (throw (Exception. "Error in data :( check "
+                     line)))
+
+(defmethod process-line :space [line]
+  (let [[last-name first-name gender fcolor dob] (str/split line #"\t")]
+    {:type     :space
+     :last     last-name
+     :first    first-name
+     :gender   gender
+     :favcolor fcolor
+     :dob      dob}))
+
+; fn to transform data into a list of hash-maps
+(defn list-of-maps [txt]
+  (let [collection-of-lines (split-by-line txt)]
+    (map process-line collection-of-lines)))
+
+
+(def commatxt (list-of-maps comma))
+(def pipetxt (list-of-maps pipe))
+(def spacetxt (list-of-maps spaces))
+
+(defn combine-all [commatxt pipetxt spacetxt]
+  (concat commatxt pipetxt spacetxt))
+
+(def all-together-now
+  (combine-all commatxt pipetxt spacetxt))
+
+;finally all the data together in one data structure
+all-together-now
 
 (comment
-  (line-based-dispatch-fn (first (split-by-line spaces)))
-
+  (process-line spaces)
+  (list-of-maps txt)
 
 
   )
+
