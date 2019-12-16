d79adb54d109b46fdb907f98fc21fa2a90cce82e
lorilynjmiller <lorilynjmiller@gmail.com>
2019-12-16 18:05:15 -0500

    -clj-time logic

--- src/address_assignment/outputs.clj
+++ src/address_assignment/outputs.clj
@@ -1,6 +1,8 @@
-(ns address-assignment.outputs)
+(ns address-assignment.outputs
+  (:require [clj-time.core :as t]
+            [clj-time.format :as f]))
 
-(def testdata [ {:type :pipe, :last "Chantrell", :first "Daryn", :gender "Female", :favcolor "Aquamarine", :dob "11/9/2019"}
+(def testdata [{:type :pipe, :last "Chantrell", :first "Daryn", :gender "Female", :favcolor "Aquamarine", :dob "11/9/2019"}
                {:type :pipe, :last "Pabst", :first "Yasmin", :gender "Female", :favcolor "Aquamarine", :dob "2/8/2019"}
                {:type :pipe, :last "McCowen", :first "Rori", :gender "Female", :favcolor "Fuscia", :dob "3/16/2019"}
                {:type :pipe, :last "Caherny", :first "Valaree", :gender "Female", :favcolor "Pink", :dob "5/8/2019"}
@@ -27,10 +29,40 @@
 (defn sort-by-genderlast [data]
   (sort-by-gender (sort-by-last-ascending data)))
 
+(def line (first testdata))
+(def date (get-in testdata [0 :dob]))
+date
+
+(f/formatter date)
+
+(def custom-formatter (f/formatter "MM/dd/yyyy"))
+
+(defn formatdate [date]
+  (f/parse custom-formatter date))
 
 
+(defn add-new-fdate-obj [data]
+  (let [newdate (formatdate (get data :dob))]
+    (assoc data :newdate newdate)))
+
+(defn add-new-fdate-all [data]
+  (map add-new-fdate-obj data))
+
+(defn convert-time [row]
+  (let [dob (get row :dob)]
+    (formatdate dob)))
+
+(defn sort-by-dob [data]
+  (sort-by convert-time data))
 
 
 (comment
-  (sort-by-genderlast testdata)
+  (def newtestdata (add-new-fdate-all testdata))
+  (formatdate date)
+  newtestdata
+  (sort-by-dob newtestdata)
+  now
+  (f/unparse basic-date now)
+  (get newtestdata :dob)
+
   )
