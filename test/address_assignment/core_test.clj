8b20ce8535e6785f417130c8e803c3576f9feaad
Lorilyn Jordan Miller <47534185+lambduhh@users.noreply.github.com>
2019-12-18 18:46:40 -0500

    Delete core_test.clj

--- test/address_assignment/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns address-assignment.core-test
-  (:require [clojure.test :refer :all]
-            [address-assignment.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
